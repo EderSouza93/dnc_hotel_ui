f67dd1d2b451a9df2edd326c17bce5ecf26a6ea7
Eder Souza <ederaldo.souza93@gmail.com>
2026-01-30 19:55:18 -0300

    feat: components pagination and form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Button from "@/component/Button";
+import TextField from "@/component/Form/TextField";
 import Link from "@/component/Link";
+import ImageField from "@/component/Form/ImageField";
+import Pagination from "@/component/Pagination";
 
 
 export default function Home() {
@@ -9,6 +12,9 @@ export default function Home() {
       <Button>clique em mim</Button>
       <Button appearance="secondary">clique em mim</Button>
       <Link href="/teste">Ir para teste</Link>
+      <TextField label="Nome completo" id="full_name" name="full_name" />
+      <ImageField label="Selecione a foto" id="profile_picture" name="profile_picture"/>
+      <Pagination currentPage={1} destination="/" totalPages={100} />
     </section>
   ) 
 }
