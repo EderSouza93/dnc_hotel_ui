b7b501a15edc820dfb1eb88ac8c8228ec52f16ba
Eder Souza <ederaldo.souza93@gmail.com>
2026-02-17 20:24:14 -0300

    feat: improve register and forgot-password component

--- src/app/cadastrar/page.tsx
+++ src/app/cadastrar/page.tsx
@@ -1,6 +1,6 @@
-
+"use client";
 import Link from "@/component/Link";
-import User
+import UserForm from "@/component/UserForm";
 
 const CadastrarPage = () => {
   return (
--- src/app/esqueci-senha/page.tsx
+++ src/app/esqueci-senha/page.tsx
@@ -1,33 +1,11 @@
-import Button from "@/component/Button";
-import TextField from "@/component/Form/TextField";
 import Link from "@/component/Link";
-import Image from "next/image";
-import { signup } from "../api/auth/signup/actions";
+import ForgotPassword from "./ForgotPassword";
 
 const EsqueciSenhaPage = () => {
   return (
     <section className="max-w-96 w-full flex justify-center items-center flex-col py-4 px-6 border border-light-grey-500 rounded-2xl">
       <span>Recuperar senha</span>
-      <form className="w-full flex flex-col items-center justify-center" action={signup}>
-        <Image
-          src="/forgot-password.svg"
-          alt="Ilustração esqueci minha senha"
-          width={172}
-          height={167}
-          className="mt-6"
-        />
-        <TextField
-          label="E-mail"
-          type="email"
-          id="email"
-          name="email"
-          className="mt-6"
-          required
-        />
-        <Button appearance="primary" type="submit" className="mt-8">
-          Enviar e-mail
-        </Button>
-      </form>
+      <ForgotPassword />
       <span className="mt-2">ou</span>
       <Link href="/login" className="my-2">
         Cancelar
