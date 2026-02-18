8aeb8d3cbb392647909f44561f70753732a86743
Eder Souza <ederaldo.souza93@gmail.com>
2026-02-17 23:15:14 -0300

    feat: First page mocked and tested on backend

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,56 +1,56 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import axios from "@/api";
-import { decodeJwt } from "jose";
+import { cookies } from "next/headers";
 
 const authOptions: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: 'E-mail', type: 'email' },
-                password: { label: 'Senha', type: 'password' }
-            },
-            async authorize(credentials) {
-                if (!credentials) throw new Error("no credentials");
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "E-mail", type: "email" },
+        password: { label: "Senha", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials) throw new Error("no credentials!");
 
-                const { data } = await axios.post("/auth/login", {
-                    email: credentials?.email,
-                    password: credentials?.password,
-                });
+        const {
+          data: { access_token },
+        } = await axios.post("/auth/login", {
+          email: credentials?.email,
+          password: credentials?.password,
+        });
 
-                const accessToken = data.access_token;
+        cookies().set("access_token", access_token);
 
-                const { sub: id } = decodeJwt(accessToken)
-                if (!id) throw new Error("token sem sub");
+        const { sub: id } = JSON.parse(
+          Buffer.from(access_token.split(".")[1], "base64").toString()
+        );
 
-                const { data: user } = await axios.get(`/users/${id}`, {
-                    headers: { Authorization: `Bearer ${accessToken}` },
-                });
+        const { data: user } = await axios.get(`/users/${id}`, {
+          headers: { Authorization: `Bearer ${access_token}` },
+        });
 
-                return {
-                    ...user,
-                    accessToken,
-                    image: user.avatar,
-                };
-            },
-        }),
-    ],
-    callbacks: {
-        async jwt({ token, user }) {
-            if (user) return {...token, ...user};
-            return token
-        },
-        async session({ session, token }) {
-            session.user = token as any;
-            return session;
-        },
+        return {
+          ...user,
+          access_token,
+          image: user.avatar,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
     },
-    pages: { signIn: '/login' },
-
-    session: { strategy: "jwt"},
-}
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+  pages: { signIn: "/login" },
+};
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,69 @@
-import Button from "@/component/Button";
-import TextField from "@/component/Form/TextField";
-import Link from "@/component/Link";
-import ImageField from "@/component/Form/ImageField";
-import Pagination from "@/component/Pagination";
-import Alert from "@/component/Alert";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { getHotels } from "./api/hotels/actions";
+import Link from "next/link";
+import Image from "next/image";
+import { getFormattedPrice } from "@/helpers/format/money";
+import Pagination from "@/component/Pagination";
 
+type SearchParams = {
+  page: string;
+  query: string;
+};
 
-export default async function Home() {
+type HomeProps = {
+  searchParams: SearchParams;
+};
+
+const LIMIT = 8;
+
+export default async function Home({ searchParams }: HomeProps) {
   const session = await getServerSession();
-  if (!session?.user) redirect("/login")
-  return(
-    <section>
-      pagina principal
-      <Button>clique em mim</Button>
-      <Button appearance="secondary">clique em mim</Button>
-      <Link href="/teste">Ir para teste</Link>
-      <TextField label="Nome completo" id="full_name" name="full_name" />
-      <ImageField label="Selecione a foto" id="profile_picture" name="profile_picture"/>
-      <Pagination currentPage={1} destination="/" totalPages={100} />
-      <Alert type="success">Esse é um feedback de sucesso</Alert>
-      <Alert type="danger">Falha na operação, tente novamennte</Alert>
-    </section>
-  ) 
+  if (!session?.user) redirect("/login");
+
+  const currentPage = Number(searchParams.page ?? 1);
+
+  const {
+    data: hotels,
+    per_page,
+    page,
+    total,
+  } = await getHotels(currentPage, LIMIT);
+
+  console.log(hotels);
+
+  return (
+    <div>
+      <section className="grid grid-cols-1 gap-2 px-5 sm:grid-cols-2 sm:px-10 md:grid-cols-3 lg:grid-cols-4 mt-20">
+        {hotels.map((hotel) => (
+          <Link href={`/hotels/${hotel.id}`} key={hotel.id}>
+            <article className="flex flex-col">
+              <div className="w-64 h-48">
+                <Image
+                  src={hotel.image ?? "/no-hotel.jpg"}
+                  width={250}
+                  height={250}
+                  quality={100}
+                  alt={`Foto do hotel ${hotel.name}`}
+                  className="object-cover rounded-3xl h-48"
+                />
+              </div>
+              <h3 className="font-bold mt-0">{hotel.name}</h3>
+              <span className="mt-2">{hotel.owner.name}</span>
+              <span className="mt-2">
+                <b>{getFormattedPrice(hotel.price)}</b> noite
+              </span>
+            </article>
+          </Link>
+        ))}
+      </section>
+      <section className="flex justify-center mt-4 mb-8">
+        <Pagination
+          currentPage={currentPage}
+          totalPages={Math.ceil(total / per_page)}
+          destination="/"
+        />
+      </section>
+    </div>
+  );
 }
